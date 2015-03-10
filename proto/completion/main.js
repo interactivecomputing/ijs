9ed7b05397ad011f426a63bbd959c9504bcbfa6f
Nikhil Kothari <nikhilko@google.com>
2015-03-10 07:09:21 -0700

    Misc

--- proto/completion/main.js
+++ proto/completion/main.js
@@ -17,6 +17,16 @@ var ternOptions = {
 };
 var ternServer = new tern.Server(ternOptions);
 
+function ternCallback(error, response) {
+  if (error) {
+    console.log('error:');
+    console.dir(error);
+  }
+  else {
+    console.dir(response);
+  }
+}
+
 function run(code, moreCode) {
   console.log('----');
   console.log(code);
@@ -29,18 +39,25 @@ function run(code, moreCode) {
     files.push({ type: 'full', name: 'moreCode', text: moreCode });
   }
 
-  ternServer.request({ query: query, files: files }, function(error, response) {
-    if (error) {
-      console.log('error:');
-      console.dir(error);
-    }
-    else {
-      console.dir(response);
-    }
-  });
+  ternServer.request({ query: query, files: files }, ternCallback);
 }
 
-run('fs = require("fs"); fs.');
-run('fs = req');
-run('fs = require("fs"); y.', 'y = 123;');
-run('s', 'str = "aaa";');
+//run('fs = require("fs"); fs.');
+//run('fs = req');
+//run('fs = require("fs"); y.', 'y = 123;');
+//run('s', 'str = "aaa";');
+
+function run2(code, moreCode) {
+  console.log('----');
+  console.log(code);
+
+  ternServer.request({files: [{type: 'full', name: 'moreCode', text: moreCode}]}, ternCallback);
+  ternServer.request({files: [{type: 'full', name: 'code', text: code}]}, ternCallback);
+  // ternServer.addFile('moreCode', moreCode);
+  // ternServer.addFile('code', code);
+
+  var query = { type: 'completions', file: 'code', end: code.length };
+  ternServer.request({ query: query }, ternCallback);
+}
+
+run2('greeting().', 'function greeting() { return "Hello"; }');
